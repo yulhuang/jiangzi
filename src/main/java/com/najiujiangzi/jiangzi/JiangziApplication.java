decc8c34b36693584509836f5f3f94db8866ce02
yul.huang <yul.huang@qq.com>
2020-04-09 14:27:13 +0800

    项目搭建

---
+++ src/main/java/com/najiujiangzi/jiangzi/JiangziApplication.java
@@ -0,0 +1,13 @@
+package com.najiujiangzi.jiangzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiangziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiangziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/najiujiangzi/jiangzi/JiangziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.najiujiangzi.jiangzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiangziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
